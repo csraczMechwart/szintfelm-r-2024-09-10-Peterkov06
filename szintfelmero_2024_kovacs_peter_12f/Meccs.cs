d47fd9a906b046352eb791d5f840e7382ab435a1
peterkov06 <peterkov06@gmail.com>
2024-09-10 10:10:35 +0200

    Uploading solution and completed "Meccs" class

---
+++ szintfelmero_2024_kovacs_peter_12f/Meccs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szintfelmero_2024_kovacs_peter_12f
+{
+    public class Meccs
+    {
+        public int Fordulo { get; set; }
+        public int Hazai { get; set; }
+        public int Vendeg { get; set; }
+        public int HazaiFel { get; set; }
+        public int VendegFel { get; set; }
+        public string HazaiNev { get; set; }
+        public string VendegNev { get; set; }
+
+        public Meccs(int fordulo, int hazai, int vendeg, int hazaiFel, int vendegFel, string hazaiNev, string vendegNev)
+        {
+            Fordulo = fordulo;
+            Hazai = hazai;
+            Vendeg = vendeg;
+            HazaiFel = hazaiFel;
+            VendegFel = vendegFel;
+            HazaiNev = hazaiNev;
+            VendegNev = vendegNev;
+        }
+    }
+}
---
+++ szintfelmero_2024_kovacs_peter_12f/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace szintfelmero_2024_kovacs_peter_12f
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Feladatok
+    {
+        private List<Meccs> Meccsek = new();
+        public Feladatok(string fileName)
+        {
+            try
+            {
+                using (StreamReader reader = new(fileName))
+                {
+                    string currLine;
+                    while ((currLine = reader.ReadLine()) != null)
+                    {
+                        string[] datas = currLine.Split(' ');
+                        Meccsek.Add(new(Convert.ToInt32(datas[0]), Convert.ToInt32(datas[1]), Convert.ToInt32(datas[2]), Convert.ToInt32(datas[3]), Convert.ToInt32(datas[4]), datas[5], datas[6]));
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public void Feladat1()
+        {
+
+        }
+    }
+}
